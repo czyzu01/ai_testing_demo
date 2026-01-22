c3a96af689994d8ea7a6899e2a438a903375d02a
Maciej Czyżowicz <maciej.czyzowicz@orange.com>
2026-01-22 21:19:35 +0100

    First example

---
+++ ai_test_eval/src/main/java/pl/orange/dop/demo/ai_test_eval/AiTestEvalApplication.java
@@ -0,0 +1,13 @@
+package pl.orange.dop.demo.ai_test_eval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTestEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTestEvalApplication.class, args);
+	}
+
+}
---
+++ ai_test_eval/src/test/java/pl/orange/dop/demo/ai_test_eval/AiTestEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.orange.dop.demo.ai_test_eval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
